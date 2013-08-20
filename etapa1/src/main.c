ae3d7aaba29ee3eb61c59f9e0067c4cf562f1579
fsoller <fsoller@gmail.com>
2013-08-20 04:04:33 -0300

    Update main.c
    - Lê do standard input ou arquivo
    - Cria um novo dicionário e adiciona os tokens enquanto houver entrada

--- etapa1/src/main.c
+++ etapa1/src/main.c
@@ -1,3 +1,9 @@
+/* Projeto de Compiladores 2013-2 - Etapa 1
+   Fernando Soller Mecking
+   Lucas Herbert Jones
+   Mateus Cardoso da Silva
+*/
+
 #include <stdio.h>
 #include "comp_dict.h"
 #include "comp_list.h"
@@ -5,17 +11,35 @@
 #include "comp_graph.h"
 #include "tokens.h"
 
-int getLineNumber (void)
-{
-  /* deve ser implementada */
-  return 0;
-}
 
 int main (int argc, char **argv)
 {
-  int token = TOKEN_ERRO;
-  while (token = yylex()){
+  int running = 1;
+  setYyin(stdin);
+
+  if (argc > 0) {
+    if (fopen(argv[1], "r"))
+    {
+        setYyin(fopen(argv[1], "r"));
+    }
+  }
+
+  dictionary = dict_new();
+
+  int token;
+
+  while (running)
+  {
+    if (!(token = yylex()) && feof(getYyin()))
+    {
+      running = 0;
+      break;
+    }
+
     printf ("token <%d> at %d\n", token, getLineNumber());
   }
+
+  dict_print(dictionary);
+
   return 0;
 }
