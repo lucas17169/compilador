4a4a61ba1be0bd2262dabc429426448f6df7d056
fsoller <fsoller@gmail.com>
2013-08-22 02:02:12 -0300

    Update comp_tree.h
    Changed function names

--- etapa0/include/comp_tree.h
+++ etapa0/include/comp_tree.h
@@ -2,13 +2,14 @@
 typedef struct comp_tree_t {
 	int data;
 	struct comp_tree_t* esq;
-	struct comp_tree_t* dir;	
+	struct comp_tree_t* dir;
 }comp_tree;
 
 
-comp_tree* add(int data, comp_tree* father_node ,comp_tree* root);
-comp_tree* delete(comp_tree* node, comp_tree* root);
-comp_tree* edit(int new_data, comp_tree* node, comp_tree* root);
-comp_tree* search(comp_tree* node, comp_tree* root);
-comp_tree* create();
-void destroy(comp_tree* root);
+comp_tree* tree_Add(int data, comp_tree* father_node ,comp_tree* root);
+comp_tree* tree_Delete(comp_tree* node, comp_tree* root);
+comp_tree* tree_Edit(int new_data, comp_tree* node, comp_tree* root);
+comp_tree* tree_Search(comp_tree* node, comp_tree* root);
+comp_tree* tree_Create();
+void tree_Destroy(comp_tree* root);
+
