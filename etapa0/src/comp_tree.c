7b30046cb0c8b36f5ac73f1b5bc96c9a3ac3503d
fsoller <fsoller@gmail.com>
2013-08-22 02:01:31 -0300

    Update comp_tree.c
    Changed function names

--- etapa0/src/comp_tree.c
+++ etapa0/src/comp_tree.c
@@ -2,58 +2,58 @@
 #include "comp_tree.h"
 
 
-comp_tree* add(int data, comp_tree* father_node, comp_tree* root){
-	if(search(father_node, root)!=NULL && (father_node->esq == NULL || father_node->dir == NULL)){
+comp_tree* tree_Add(int data, comp_tree* father_node, comp_tree* root){
+	if(tree_Search(father_node, root)!=NULL && (father_node->esq == NULL || father_node->dir == NULL)){
 		if(father_node->esq == NULL){
 			comp_tree *new_node = malloc( sizeof(comp_tree));
-			father_node->esq = new_node; 
+			father_node->esq = new_node;
 			return new_node;
 		}
 		else{
 			comp_tree *new_node = malloc( sizeof(comp_tree));
-			father_node->dir = new_node; 
+			father_node->dir = new_node;
 			return new_node;
 		}
-		
+
 	}
 	else return NULL;
 }
 
-comp_tree* delete(comp_tree* node, comp_tree* root){
-		if(search(node, root)!=NULL && (node->esq == NULL && node->dir == NULL)){
+comp_tree* tree_Delete(comp_tree* node, comp_tree* root){
+		if(tree_Search(node, root)!=NULL && (node->esq == NULL && node->dir == NULL)){
 			free(node);
 			return root;
 		}
 		else return NULL;
-			
+
 }
 
-comp_tree* edit(int new_data, comp_tree* node, comp_tree* root){
-	if(search(node, root)!=NULL){
+comp_tree* tree_Edit(int new_data, comp_tree* node, comp_tree* root){
+	if(tree_Search(node, root)!=NULL){
 		node->data = new_data;
 	}
 	return node;
 }
 
-comp_tree* search(comp_tree* node, comp_tree* root){
+comp_tree* tree_Search(comp_tree* node, comp_tree* root){
 	if(root == NULL)
 		return NULL;
 	else{
 		if(node == root)
 			return node;
-		else if(search(node, root->esq) != NULL || search(node, root->dir) != NULL)
+		else if(tree_Search(node, root->esq) != NULL || tree_Search(node, root->dir) != NULL)
 				return node;
-			else return NULL;		
+			else return NULL;
 	}
 }
 
-comp_tree* create(){
+comp_tree* tree_Create(){
 	return NULL;
 }
-void destroy(comp_tree* root){
+void tree_Destroy(comp_tree* root){
 	if(root!=NULL){
-		destroy(root->esq);
-		destroy(root->dir);
+		tree_Destroy(root->esq);
+		tree_Destroy(root->dir);
 		free(root);
 	}
 }
